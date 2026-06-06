dec0ba0d0049ef9410eedddcfb90ecc5f9496b47
Shem Buaron <shembuaron@gmail.com>
2026-06-06 12:09:29 +0300

    Create myLibrary.groovy

---
+++ vars/myLibrary.groovy
@@ -0,0 +1,11 @@
+def buildApp() {
+    echo "Now building the application..."
+}
+
+def deployApp(String branchName) {
+    echo "Now deploying the application on branch: ${branchName}..."
+}
+
+def cleanup() {
+    echo "Cleaning up after build and deployment..."
+}
